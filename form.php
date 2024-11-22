b6b0f321cf512c5b9bb24fb3bd13f419a5bba56f
prxnn <pronpanpronpan5@gmail.com>
2024-11-22 16:39:47 +0700

    22/11/2567

---
+++ form.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<body class="vh-100" style="align-content: center;
+}">
+    <form action="insert_into.php" method="post" enctype="multipart-form/data">
+        <div class="row" style="justify-content: center;">
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 style="    place-self: center;"></h3>
+                        <hr>
+                        <div class="row">
+                            <div class="col-6">
+                                <label for="" class="form-label"></label>
+                                <input type="text" class="form-control">
+
+                            </div>
+                            <div class="col-6">
+                                <label for="" class="form-label"></label>
+                                <input type="text" class="form-control">
+
+                            </div>
+                            </div>
+                            <div class="col-6">
+                                <label for="" class="form-label"></label>
+                                <input type="text" class="form-control">
+
+                            </div>
+                            
+                            
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+    </form>
+</body>
+<script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+</html>
---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<style>
+    .link {
+        color: black;
+
+    }
+
+    .link:hover {
+
+        color: red;
+    }
+</style>
+
+<body class="vh-100">
+    <div class="d-flex " style="justify-content:space-around;">
+        <ul class="nav  ">
+            <li class="nav-item m-2">
+                <h5 class="nav-link active text-dark">ร้านซูชิ
+                </h5>
+            </li>
+        </ul>
+
+        <ul class="nav justify-content-end ">
+
+            <li class="nav-item m-2">
+                <a href="http://" class="nav-link active link">หน้าแรก
+                </a>
+            </li>
+            <li class="nav-item m-2">
+                <a href="http://" class="nav-link active link">เมนู
+                </a>
+            </li>
+            <li class="nav-item m-2">
+                <a href="http://" class="nav-link active link">โปรโมชั่น
+                </a>
+            </li>
+            <li class="nav-item m-2">
+                <a href="http://" class="nav-link active link">เกี่ยวกับเรา
+                </a>
+            </li>
+            <li class="nav-item m-2">
+                <a href="http://" class="nav-link active link">ติดต่อ
+                </a>
+            </li>
+        </ul>
+
+    </div>
+    <img src="img/แบนเนอร์.png"
+        alt="" class="img-fluid">
+
+</body>
+<script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+</html>
